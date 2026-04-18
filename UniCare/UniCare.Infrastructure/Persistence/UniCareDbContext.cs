d418d8f86c574ad9d1b5ea289bf926040818c5b4
T0TA-MAGDY <youstena.ma@gmail.com>
2026-04-18 15:15:09 +0200

    added item infrastructure

---
+++ UniCare/UniCare.Infrastructure/Persistence/UniCareDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UniCare.Domain.Aggregates.ItemAggregates;
+
+namespace UniCare.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
