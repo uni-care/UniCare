aeb9a89fcf2914c230e77575e0e236a777d4741c
T0TA-MAGDY <youstena.ma@gmail.com>
2026-04-18 14:38:44 +0200

    added item domain

--- UniCare/UniCare.Domain/Common/BaseEntity.cs
+++ UniCare/UniCare.Domain/Common/BaseEntity.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace UniCare.Domain.Common
 {
-    internal class BaseEntity
+    public abstract class BaseEntity
     {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
     }
 }
