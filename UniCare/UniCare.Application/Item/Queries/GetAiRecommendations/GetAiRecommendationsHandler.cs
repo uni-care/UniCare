43ebe972ef9813f392d3fa9a3b7e1c88ca294833
T0TA-MAGDY <youstena.ma@gmail.com>
2026-07-21 20:23:19 +0300

    handler works with repo instead of touching the context directly

--- UniCare/UniCare.Application/Item/Queries/GetAiRecommendations/GetAiRecommendationsHandler.cs
+++ UniCare/UniCare.Application/Item/Queries/GetAiRecommendations/GetAiRecommendationsHandler.cs
@@ -1,7 +1,7 @@
 ﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
 using UniCare.Application.Interfaces;
 using UniCare.Application.Item.DTOs;
+using UniCare.Domain.Aggregates.ItemAggregates;
 using UniCare.Domain.Interfaces;
 
 namespace UniCare.Application.Item.Queries.GetAiRecommendations
@@ -9,12 +9,12 @@ namespace UniCare.Application.Item.Queries.GetAiRecommendations
     public class GetAiRecommendationsHandler : IRequestHandler<GetAiRecommendationsQuery, List<ItemDto>>
     {
         private readonly IAiRecommendationService _aiService;
-        private readonly IApplicationDbContext _context;
+        private readonly IItemRepository _itemRepository;
 
-        public GetAiRecommendationsHandler(IAiRecommendationService aiService, IApplicationDbContext context)
+        public GetAiRecommendationsHandler(IAiRecommendationService aiService, IItemRepository itemRepository)
         {
             _aiService = aiService;
-            _context = context;
+            _itemRepository = itemRepository;
         }
 
         public async Task<List<ItemDto>> Handle(GetAiRecommendationsQuery request, CancellationToken ct)
@@ -23,11 +23,7 @@ namespace UniCare.Application.Item.Queries.GetAiRecommendations
 
             if (!ids.Any()) return new List<ItemDto>();
 
-            var itemsList = await _context.Items
-                .Include(i => i.Owner)
-                .Include(i => i.Category)
-                .Where(i => ids.Contains(i.Id))
-                .ToListAsync(ct);
+            var itemsList = await _itemRepository.GetByIdsAsync(ids, ct);
 
             var items = itemsList.Select(i => ItemDtoMapper.Map(i)).ToList();
 
